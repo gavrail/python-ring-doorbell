06d1983aa89e13bc03fabefb2c4310d8701fe46b
Gavrail Tatarliev <g.tatarliev@qvii.com>
2025-10-27 14:17:16 -0400

    take screenshot on movement idea

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import json
 from pathlib import Path
 from pprint import pprint
 
-from ring_doorbell import Auth, AuthenticationError, Requires2FAError, Ring
+from ring_doorbell import Auth, AuthenticationError, Requires2FAError, Ring, RingEventListener
 
 user_agent = "YourProjectName-1.0"  # Change this
 cache_file = Path(user_agent + ".token.cache")
@@ -31,6 +31,19 @@ async def do_auth():
     return auth
 
 
+
+
+class SnapShotter(object):
+
+    def __init__(self, doorbell):
+        self.doorbell = doorbell
+
+    def on_event(self, event):
+        if event.kind == "motion":
+            print("Motion detected!", event)
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(self.doorbell.async_get_snapshot(filename="snapshot.jpg"))
+
 async def main() -> None:
     if cache_file.is_file():  # auth token is cached
         auth = Auth(user_agent, json.loads(cache_file.read_text()), token_updated)
@@ -45,15 +58,18 @@ async def main() -> None:
 
     await ring.async_update_data()
 
+    listener = RingEventListener(ring)
+
+    await listener.start()
+
     devices = ring.devices()
     doorbell = devices['doorbots'][0]
-    try:
-        await doorbell.async_recording_download(
-            (await doorbell.async_history(limit=100, kind='ding'))[0]['id'],
-                            filename='last_ding.mp4',
-                            override=True)
-    finally:
-        await auth.async_close()
+    snap_shotter = SnapShotter(doorbell)
+    listener.add_notification_callback(snap_shotter.on_event)
+
+    await asyncio.Event().wait()
+
+    await auth.async_close()
 
 
 if __name__ == "__main__":
